7e73496f62647711e25fe72cec16545804a6876d
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-01-23 12:13:53 +0100

    Visuals and input receiver done

---
+++ dist/scripts/canvas.js
@@ -0,0 +1,57 @@
+/* eslint-disable import/extensions */
+import {
+  constantColumns,
+  constantRows,
+  constantBoxSize,
+  constantPrimaryColor,
+  constantDeadColor,
+} from "./constants.js";
+
+class BoardPencil {
+  myCanvas;
+  context;
+  arrayBoard;
+  numberOfColumns = constantColumns;
+  numberOfRows = constantRows;
+  boxsize = constantBoxSize;
+  primaryColor = constantPrimaryColor;
+  deadColor = constantDeadColor;
+  constructor(canvas, gameArray) {
+    this.myCanvas = canvas;
+    this.arrayBoard = gameArray;
+    this.context = this.myCanvas.getContext("2d");
+    this.myCanvas.width = this.numberOfColumns * this.boxsize;
+    this.myCanvas.height = this.numberOfRows * this.boxsize;
+  }
+
+  drawBoard() {
+    // that axes wer inverted CAUTION
+    for (let i = 0; i < this.numberOfRows; i++) {
+      for (let j = 0; j < this.numberOfColumns; j++) {
+        if (this.arrayBoard[i][j] === true) {
+          this.drawSquare(i, j, this.primaryColor);
+        }
+        this.drawSquare(j, i, this.deadColor);
+      }
+    }
+  }
+
+  drawSquare(x, y, color) {
+    this.context.fillStyle = color;
+    this.context.fillRect(
+      x * this.boxsize,
+      y * this.boxsize,
+      this.boxsize,
+      this.boxsize
+    );
+    this.context.strokeStyle = "rgb(156, 156, 156)";
+    this.context.strokeRect(
+      x * this.boxsize,
+      y * this.boxsize,
+      this.boxsize,
+      this.boxsize
+    );
+  }
+}
+
+export default BoardPencil;
---
+++ dist/scripts/main.js
@@ -0,0 +1,80 @@
+/* eslint-disable import/extensions */
+import BoardPencil from "./canvas.js";
+import traverse2dArray from "./cycle-board.js";
+import {
+  constantColumns,
+  constantRows,
+  constantBoxSize,
+  constantPrimaryColor,
+  constantDeadColor,
+} from "./constants.js";
+
+/* const neighborFile = require("./neighbor-detector"); */
+/* const neighborCounter = neighborFile; */
+
+const create2dArray = (rows, colums) => {
+  const array2d = [];
+  for (let i = 0; i < rows; i++) {
+    array2d[i] = [];
+    for (let j = 0; j < colums; j++) {
+      array2d[i][j] = false;
+    }
+  }
+  return array2d;
+};
+
+const mouse = {
+  x: 0,
+  y: 0,
+};
+
+const size = {
+  height: window.innerHeight,
+  width: window.innerWidth,
+};
+
+const selectReviveCell = (x, y, table, pencil) => {
+  if (table[y][x] === true) {
+    pencil.drawSquare(x, y, constantDeadColor);
+  } else {
+    pencil.drawSquare(x, y, constantPrimaryColor);
+  }
+};
+
+// Execution
+
+const gameArray = create2dArray(constantRows, constantColumns);
+console.table(gameArray);
+const mainCanvas = document.querySelector(".game-view-port");
+const myPencil = new BoardPencil(mainCanvas, gameArray);
+myPencil.drawBoard();
+
+traverse2dArray(gameArray);
+
+// Event listeners
+
+window.addEventListener("mousemove", (event) => {
+  mouse.x = Number.parseInt(
+    (event.clientX -
+      (size.width / 2 - (constantColumns * constantBoxSize) / 2)) /
+      constantBoxSize,
+    10
+  );
+  mouse.y = Number.parseInt(
+    (event.clientY - (size.height / 2 - (constantRows * constantBoxSize) / 2)) /
+      constantBoxSize,
+    10
+  );
+});
+
+window.addEventListener("click", () => {
+  selectReviveCell(mouse.x, mouse.y, gameArray, myPencil);
+  if (gameArray[mouse.y][mouse.x] === true) {
+    gameArray[mouse.y][mouse.x] = false;
+  } else {
+    gameArray[mouse.y][mouse.x] = true;
+  }
+});
+
+/* module.exports.creade2dArray = create2dArray;
+module.exports.traverse2dArray = traverse2dArray; */
